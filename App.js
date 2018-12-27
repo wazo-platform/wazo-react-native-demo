5f2239bbe9946b95e5d633b4cde761163ddf7509
Emmanuel Quentin <manu.quentin@gmail.com>
2018-12-27 14:31:10 -0500

    Add react-native-callkeep on ios

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import RNCallKeep from 'react-native-callkeep';
+import uuid from 'uuid';
 
-import {
-  RTCPeerConnection,
-  RTCSessionDescription,
-  MediaStream,
-  getUserMedia,
-} from 'react-native-webrtc';
+import { RTCPeerConnection, RTCSessionDescription, MediaStream, getUserMedia } from 'react-native-webrtc';
 
 import { WazoApiClient, WazoWebRTCClient } from '@wazo/sdk';
 
@@ -27,45 +24,79 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   button: {
-    paddingTop: 20
-  }
+    paddingTop: 20,
+  },
 });
 
 export default class App extends React.Component {
+
   constructor(props) {
     super(props);
 
     this.webRtcClient = null;
+    this.currentCallId = null;
+    this.currentSession = null;
 
     this.state = {
-      server: 'demo.wazo.io',
-      username: '',
-      password: '',
-      number: '',
-      connected: false
-    }
+      server: 'demo.wazo.community',
+      username: 'equentin@wazo.io',
+      password: 'secret',
+      number: '8008',
+      connected: false,
+      ringing: false,
+      inCall: false,
+      error: null,
+    };
   }
-  
-  authenticate() {
+
+  initializeCallKeep = () => {
+    // Initialise RNCallKit
+    const options = {
+      appName: 'WazoReactNativeDemo',
+    };
+
+    try {
+      RNCallKeep.setup(options);
+    } catch (err) {
+      console.error('initializeCallKeep error:', err.message);
+    }
+
+    // Add RNCallKit Events
+    RNCallKeep.addEventListener('didReceiveStartCallAction', this.onNativeCall);
+    RNCallKeep.addEventListener('answerCall', this.onAnswerCallAction);
+    RNCallKeep.addEventListener('endCall', this.onEndCallAction);
+    RNCallKeep.addEventListener('didDisplayIncomingCall', this.onIncomingCallDisplayed);
+    RNCallKeep.addEventListener('didPerformSetMutedCallAction', this.onToggleMute);
+  };
+
+  authenticate = () => {
     const { server, username, password } = this.state;
     const apiClient = new WazoApiClient({ server });
 
-    apiClient.auth.logIn({ username, password }).then((data) => {
-      const userToken = data.token;
+    apiClient.auth
+      .logIn({ username, password })
+      .then(data => {
+        const userToken = data.token;
 
-      apiClient.confd.getUser(userToken, data.uuid).then((user) => {
-        const line = user.lines[0];
+        apiClient.confd
+          .getUser(userToken, data.uuid)
+          .then(user => {
+            const line = user.lines[0];
 
-        console.log('user connected', line);
-
-        apiClient.confd.getUserLineSip(data.token, data.uuid, line.id).then((sipLine) => {
-          this.initializeWebRtc(sipLine, server);
-        }).catch(console.log);
-      }).catch(console.log);
-    }).catch(console.log);
+            apiClient.confd
+              .getUserLineSip(data.token, data.uuid, line.id)
+              .then(sipLine => {
+                this.initializeWebRtc(sipLine, server);
+                this.initializeCallKeep();
+              })
+              .catch(console.log);
+          })
+          .catch(console.log);
+      })
+      .catch(console.log);
   };
 
-  initializeWebRtc(sipLine, host) {
+  initializeWebRtc = (sipLine, host) => {
     this.webRtcClient = new WazoWebRTCClient({
       host,
       displayName: 'My dialer',
@@ -73,28 +104,94 @@ export default class App extends React.Component {
       password: sipLine.secret,
       uri: sipLine.username + '@' + host,
       media: {
-        audio: true
-      }
+        audio: true,
+      },
     });
 
-    this.webRtcClient.on('invite', (session) => {
-      try {
-        this.webRtcClient.answer(session);
-      }catch (e) {
-        console.log('invite error', e);
-      }
-    });
+    this.webRtcClient.on('invite', session => {
+      this.setupCallSession(session);
+      this.setState({ ringing: true });
 
-    this.webRtcClient.on('*', function (data, eventName) {
-      console.log('rtc', eventName, data);
+      // Tell callkit that we a call is incoming
+      RNCallKeep.displayIncomingCall(this.getCurrentCallId(), this.webRtcClient.getNumber(session));
     });
 
     this.setState({ connected: true });
   };
-  
-  call() {
-    this.webRtcClient.call(this.state.number);
-  }
+
+  setupCallSession = session => {
+    this.currentSession = session;
+
+    session.on('failed', (response, cause) => {
+      this.setState({ error: cause, ringing: false, inCall: false });
+    });
+
+    session.on('terminated', () => {
+      this.hangup();
+    });
+  };
+
+  call = (number) => {
+    const session = this.webRtcClient.call(number);
+    this.setupCallSession(session);
+
+    this.setState({ inCall: true, ringing: false });
+
+    // Tell callkit that we are in call
+    RNCallKeep.startCall(this.getCurrentCallId(), number);
+  };
+
+  answer = () => {
+    this.setState({ inCall: true, ringing: false });
+
+    this.webRtcClient.answer(this.currentSession);
+  };
+
+  hangup = () => {
+    const currentCallId = this.getCurrentCallId();
+    if (!this.currentSession || !currentCallId) {
+      return;
+    }
+
+    this.webRtcClient.hangup(this.currentSession);
+
+    RNCallKeep.endCall(currentCallId);
+    this.setState({ inCall: false, ringing: false });
+    this.currentCallId = null;
+    this.currentSession = null;
+  };
+
+  getCurrentCallId = () => {
+    if (!this.currentCallId) {
+      this.currentCallId = uuid.v4();
+    }
+
+    return this.currentCallId;
+  };
+
+  onAnswerCallAction = ({ callUUID }) => {
+    // called when the user answer the incoming call
+    this.answer();
+  };
+
+  onIncomingCallDisplayed = error => {
+    // You will get this event after RNCallKeep finishes showing incoming call UI
+    // You can check if there was an error while displaying
+  };
+
+  onNativeCall = ({ handle }) => {
+    // Called when performing call from native Contact app
+    this.call(handle);
+  };
+
+  onEndCallAction = ({ callUUID }) => {
+    this.hangup();
+  };
+
+  onToggleMute = (muted) => {
+    // Called when the system or the user mutes a call
+    this.webRtcClient[muted ? 'mute' : 'unmute'](this.currentSession);
+  };
 
   render() {
     const { connected, server } = this.state;
@@ -103,26 +200,13 @@ export default class App extends React.Component {
       <View style={styles.container}>
         {!connected && (
           <React.Fragment>
-            <TextInput
-              autoCapitalize="none"
-              onChangeText={(username) => this.setState({ username })}
-              value={this.state.username}
-              placeholder="Username"
-            />
-            <TextInput
-              autoCapitalize="none"
-              onChangeText={(password) => this.setState({ password })}
-              value={this.state.password}
-              placeholder="Password"
-            />
-            <TextInput
-              autoCapitalize="none"
-              defaultValue={server}
-              onChangeText={(server) => this.setState({ server })}
-              placeholder="Server"
-            />
+            <TextInput autoCapitalize="none" onChangeText={username => this.setState({ username })} placeholder="Username" value={this.state.username} />
+            <TextInput autoCapitalize="none" onChangeText={password => this.setState({ password })} placeholder="Password" value={this.state.password} />
+            <TextInput autoCapitalize="none" defaultValue={server} onChangeText={server => this.setState({ server })} placeholder="Server" />
 
-            <TouchableOpacity style={styles.button} onPress={this.authenticate.bind(this)}><Text>Login</Text></TouchableOpacity>
+            <TouchableOpacity onPress={this.authenticate.bind(this)} style={styles.button}>
+              <Text>Login</Text>
+            </TouchableOpacity>
           </React.Fragment>
         )}
 
@@ -131,16 +215,31 @@ export default class App extends React.Component {
             <TextInput
               autoCapitalize="none"
               keyboardType="numeric"
-              onChangeText={(number) => this.setState({ number })}
+              onChangeText={number => this.setState({ number })}
               onSubmitEditing={this.call.bind(this)}
-              value={this.state.number}
               placeholder="Number"
+              value={this.state.number}
             />
 
-            <TouchableOpacity style={styles.button} onPress={this.call.bind(this)}><Text>Call</Text></TouchableOpacity>
+            {!this.state.ringing && !this.state.inCall && (
+              <TouchableOpacity onPress={() => this.call(this.state.number)} style={styles.button}>
+                <Text>Call</Text>
+              </TouchableOpacity>
+            )}
+            {this.state.ringing && (
+              <TouchableOpacity onPress={this.answer} style={styles.button}>
+                <Text>Answer</Text>
+              </TouchableOpacity>
+            )}
+            {this.state.inCall && (
+              <TouchableOpacity onPress={this.hangup} style={styles.button}>
+                <Text>Hangup</Text>
+              </TouchableOpacity>
+            )}
           </React.Fragment>
         )}
       </View>
     );
   }
+
 }
--- ios/WazoReactNativeDemo/AppDelegate.m
+++ ios/WazoReactNativeDemo/AppDelegate.m
@@ -9,27 +9,37 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNCallKeep.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"WazoReactNativeDemo"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void(^)(NSArray * __nullable restorableObjects))restorationHandler
+{
+  return [RNCallKeep application:application
+            continueUserActivity:userActivity
+              restorationHandler:restorationHandler];
 }
 
 @end
